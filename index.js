9b7c0e42a2eff28a37f86b7d860edd651cc6f222
201580ag <39054577+201580ag@users.noreply.github.com>
2024-04-12 23:28:22 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i)=>{
+    a.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShBGdQIVTUFpEOHnSXeufUir40LI0jofvbqtHEpt0oSQ&s'
+})
+}, 500);
+
